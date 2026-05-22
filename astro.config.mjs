308a913809f3d4a988d216266c4478812ad6907b
ssamucr <ssamucr@gmail.com>
2026-05-21 21:42:44 -0500

    Implemented some optimizations to product images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alas-catalogo.samu0x.dev/',
+  prefetch: {
+    defaultStrategy: 'hover',
+    prefetchAll: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
