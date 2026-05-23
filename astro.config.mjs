9748007644d4c06b2ef5e0f3533fbb3657eb113a
ssamucr <ssamucr@gmail.com>
2026-05-23 16:01:29 -0500

    Reverted last changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
-import { defineConfig, sharpImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alas-catalogo.samu0x.dev/',
-  image: {
-    service: sharpImageService(),
-  },
   prefetch: {
     defaultStrategy: 'hover',
     prefetchAll: false,
